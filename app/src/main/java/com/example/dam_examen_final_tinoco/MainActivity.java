e934a840fff58d7633ea313e571a45107ad66870
VictorANT200 <20a3110452@untels.edu.pe>
2024-12-16 15:17:46 -0500

    LOGIN

---
+++ app/src/main/java/com/example/dam_examen_final_tinoco/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.dam_examen_final_tinoco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn1 = findViewById(R.id.Boton_registrarse);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "REGISTRO COMPLETADO", Toast.LENGTH_SHORT).show());
+
+
+    }
+}
